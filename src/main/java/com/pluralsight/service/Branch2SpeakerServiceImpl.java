166946ebfdadd39c2b6074a2a0b1f256d31cf283
Jose Luis Mesa <jose.mesa@6connex.com>
2020-11-02 08:16:01 -0500

    Adding a speaker

---
+++ src/main/java/com/pluralsight/service/Branch2SpeakerServiceImpl.java
@@ -0,0 +1,13 @@
+package com.pluralsight.service;
+
+import com.pluralsight.model.Speaker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Branch2SpeakerServiceImpl implements SpeakerService {
+    @Override
+    public List<Speaker> findAll() {
+        return new ArrayList<>();
+    }
+}
